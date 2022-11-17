2f64a1204a7deab285c5c0d5e32a276eac8f58c9
maheralsamman <supermaher2@gmail.com>
2022-11-18 00:07:01 +0100

    add suggested word

--- src/components/TodaysMovies.svelte
+++ src/components/TodaysMovies.svelte
@@ -1,26 +1,24 @@
 <script>
-    import MovieCard from "./MovieCard.svelte"
-
-export let todays;
-console.log(todays);
+	import MovieCard from './MovieCard.svelte';
+	export let todays;
+	export let word;
 </script>
 
-<h3>Movies of the day</h3>
+<h3>Movies of the day is: {word}</h3>
 <div class="todays-movies">
-    {#each todays as movie (movie.imdbID)}
-    <MovieCard {movie}/>
-    {/each}
-
+	{#each todays as movie (movie.imdbID)}
+		<MovieCard {movie} />
+	{/each}
 </div>
 
 <style>
-    h3 {
-        margin-left: 0.8em;
-    }
-.todays-movies{
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    grid-column-gap:1rem;
-    grid-row-gap:2rem
-}
+	h3 {
+		margin-left: 0.8em;
+	}
+	.todays-movies {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		grid-column-gap: 1rem;
+		grid-row-gap: 2rem;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
 	import SearchMovie from '../components/SearchMovie.svelte';
 	import { fly } from 'svelte/transition';
 	export let data;
-	const { todays } = data.props;
+	const { todays, word } = data.props;
 	//console.log(todaysMovies);
 </script>
 
 <section in:fly={{ y: 50, duration: 500, delay: 500}} out:fly={{ duration: 500 }}>
 	<SearchMovie />
-	<TodaysMovies {todays} />
+	<TodaysMovies {todays} {word} />
 </section>
