babc35ae172db4cd9766ba8b05b3eaecd6e2d59c
maheralsamman <supermaher2@gmail.com>
2022-11-18 00:35:13 +0100

    first commit

--- src/routes/movie/[id]/+page.js
+++ src/routes/movie/[id]/+page.js
@@ -1,4 +1,4 @@
-import { PUBLIC_API_KEY } from '$env/static/public'
+import { PUBLIC_API_KEY } from '$env/static/public';
 export async function load({ params }) {
 	const { id } = params;
 	const res = await fetch(`https://www.omdbapi.com/?apikey=${PUBLIC_API_KEY}&i=${id}`);
